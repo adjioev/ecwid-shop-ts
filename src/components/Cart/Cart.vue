515a8ab0798d7fc41a95169fb9b662e17df6c3af
Alexandre Djioev <adjioev@vervio.com.au>
2024-09-27 20:35:06 +0400

    clean up

--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -38,8 +38,7 @@ const handleModalClose = () => {
     <button type="button" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="handleModalClose">Go back to shopping</button>
   </div>
 </Modal>
-<CartComponent :items="cartItems"
-               @remove-from-cart="handleItemRemove" />
+<CartComponent :items="cartItems" @remove-from-cart="handleItemRemove" />
 </template>
 
 <style scoped>
