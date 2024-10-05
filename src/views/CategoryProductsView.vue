fea97746bd3c65f0ee910ebb646d77061bbae63a
Alexandre Djioev <adjioev@vervio.com.au>
2024-10-05 20:02:35 +0400

    another clean up

--- src/views/CategoryProductsView.vue
+++ src/views/CategoryProductsView.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import {computed, ref} from 'vue';
 import BreadcrumbsComponent from "@/components/Breadcrumbs/BreadcrumbsComponent.vue";
 import ProductGrid from "@/components/ProductGrid/ProductGrid.vue";
 import { useCategoryStore } from '@/stores/categoryStore';
+import LoadingComponent from "@/components/Misc/LoadingComponent.vue";
 
 const categoryStore = useCategoryStore();
 
 const props = defineProps<{
   id: string | undefined;
 }>();
+
+const loading = ref(true);
+
+const handleLoadingStart = () => {
+  loading.value = true;
+};
+const handleLoadingStop = () => {
+  loading.value = false;
+};
 
 const categoryName = computed(() => {
   const categoryId = Number(props.id);
@@ -21,9 +31,12 @@ const pages = computed(() => [
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl pb-8">{{ categoryName }}</h1>
-  <BreadcrumbsComponent :pages="pages"/>
-  <ProductGrid :categoryId="props.id" />
+  <LoadingComponent v-if="loading" />
+  <div v-show="!loading">
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl pb-8">{{ categoryName }}</h1>
+    <BreadcrumbsComponent :pages="pages"/>
+    <ProductGrid :categoryId="props.id"  @start-loading="handleLoadingStart" @stop-loading="handleLoadingStop" />
+  </div>
 </template>
 
 <style scoped>
--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import {useCategoryStore} from "@/stores/categoryStore.ts";
+import LoadingComponent from "@/components/Misc/LoadingComponent.vue";
 import ProductGrid from "@/components/ProductGrid/ProductGrid.vue";
 import CategoryList from "@/components/CategoryList/CategoryList.vue";
-import {useCategoryStore} from "@/stores/categoryStore.ts";
-import {ref} from "vue";
 
 const loading = ref(true);
 
@@ -16,9 +17,7 @@ const handleLoadingStop = () => {
 </script>
 
 <template>
-  <div v-if="loading" class="flex items-center justify-center">
-    <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500 mt-20" ></div>
-  </div>
+  <LoadingComponent v-if="loading" />
   <div v-show="!loading">
     <div class="pb-16">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl py-4">Categories</h1>
